4278d320d7e1b74346520b08e514848dba49c16c
ljy2855 <ljy2855@naver.com>
2020-04-18 17:53:36 +0900

    db 업뎃
    자동으로 데이터 모집

--- fun.py
+++ fun.py
@@ -2,11 +2,12 @@ import requests
 import re
 import sqlite3
 import time
+import json
 
 from module import sohwan
 
 
-api_key = 'RGAPI-be6d927c-255e-42fd-9cdb-fcfcde0c1a78'
+api_key = 'RGAPI-6c7b0608-1a34-43b4-ae37-4a83550e0328'
 season = '13'
 
 def get_id(user_name):
@@ -23,12 +24,14 @@ def update_db(user_name):
     print(query)
     cur.execute(query)
     summ = get_id(user_name)
-    match = get_current_match(sohwan)
+    match = get_current_match(summ.accountID)
+    str_match = json.dumps(match)
+    
     if not cur.fetchall()   :
-        cur.execute("insert into USER_LIST(name,id,accountID,update_time,match) VALUES(?,?,?,?,?);",(summ.name,summ.id,summ.accountID,int(time.time()),sqlite3.Binary(match)))
+        cur.execute("insert into USER_LIST(name,id,accountID,update_time,match) VALUES(?,?,?,?,?);",(summ.name,summ.id,summ.accountID,int(time.time()),str_match))
     else :
         cur.execute("update USER_LIST "
-        "set id = '%s', accountID = '%s', update_time = %d where name = '%s';"%(summ.id,summ.accountID,int(time.time()),user_name,))
+        "set id = '%s', accountID = '%s', update_time = %d,match = '%s' where name = '%s';"%(summ.id,summ.accountID,int(time.time()),str_match,user_name,))
 
     
     con.commit()
@@ -51,24 +54,48 @@ def active_game(user_name):
         print(new[index].name)
         index += 1
 
+def all_game():
+    url = 'https://kr.api.riotgames.com/lol/spectator/v4/featured-games?api_key=' + api_key
+    r = requests.get(url)
+    usr_list = []
+    cnt = 0
+    flag =1
+    for i in r.json()['gameList']:
+        for j in i['participants']:
+            usr_list.append(j['summonerName'])
+            print(j['summonerName'])
+            if cnt == 100:
+                flag = 0
+                break
+            cnt += 1
+        if flag == 0:
+            break        
+    for i in usr_list:
+        update_db(i)
+
+
+
 #소환사 리스트 뽑기까지
 def dodge():
     summ = []
     for i in range(5):
         line = input()
-        index = line.find("님")
+        index = line.find("님이 방에 참가했습니다.")
         summ.append(get_id(line[0:index]))
-        get_current_match(summ[i])
+        update_db(summ[i].name)
+        print(summ[i].name)
 
 
     
 
-def get_current_match(sohwan):
-    url = 'https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/' + sohwan.accountID '?queue=420&queue440&api_key=' + api_key
-    r = requests.get(url)
-    match = r.json()['matches']
-    return match
-
+def get_current_match(accountID):
+    url = 'https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/' + accountID + '?queue=420&queue=440&season=13&endIndex=50&api_key=' + api_key
+    try :
+        r = requests.get(url)
+        match = r.json()['matches']
+        return match
+    except :
+        print("최근 매치를 확인할수 없습니다.")
 
 
 
@@ -97,4 +124,9 @@ def count_game(user_name,targetSeason):
         totalGame += 100
 
     print(totalGame)
+
+if __name__ == '__main__':
+    all_game()
+    
+
 
